d71a2b9fd118c27176b4dfd9375342020ec3faba
azalahmadkhan <azalahmadkhan@gmail.com>
2025-09-15 16:10:46 -0500

    updated code for 8B

--- qwen3_0.6_AIME.py
+++ qwen3_0.6_AIME.py
@@ -22,7 +22,7 @@ model_path = "./Qwen3-8B"
 
 sampling_params = SamplingParams(
     temperature=0.0,   # deterministic output
-    max_tokens=512
+    max_tokens=1024
 )
 
 # ===============================
@@ -78,14 +78,14 @@ llm = LLM(
     model=model_path,
     tensor_parallel_size=1,
     gpu_memory_utilization=0.85,
-    max_model_len=10000
+    max_model_len=17000
 )
 
 for idx, row in tqdm(df.iterrows(), total=len(df), desc="Evaluating"):
     problem = str(row["problem"]).strip()
     expected = int(row["answer"])  # guaranteed integer
 
-    prompt = f"""Solve the following math problem **step by step**. 
+    prompt = f"""Solve the following math problem.
 At the end, write the **final numeric answer only** on a new line after the exact phrase:
 
 Final Answer:
